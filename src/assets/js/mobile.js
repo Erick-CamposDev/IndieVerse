a3e0ca87c904916ceec30f1b006013c424340144
Erick Fellipe <efrc22br@gmail.com>
2025-10-12 15:56:39 -0300

    Adicionado menu de tab buttons para dispositivos de touch

--- src/assets/js/mobile.js
+++ src/assets/js/mobile.js
@@ -2,10 +2,17 @@ const menuMobile = document.getElementById("menuMobile");
 const menuBtn = document.getElementById("menuBtn");
 const closeBtn = document.getElementById("closeBtn");
 
+const tabModalBtn = document.getElementById("tabMenuBtn");
+const tabMenu = document.querySelector(".tab-menu-mobile");
+
 menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("active");
 });
 
 closeBtn.addEventListener("click", () => {
   menuMobile.classList.remove("active");
+});
+
+tabModalBtn.addEventListener("click", () => {
+  tabMenu.classList.toggle("active");
 });
