7bcede6142ab5ff38cb117930991103eb6277b11
Erick Fellipe <efrc22br@gmail.com>
2025-10-15 15:59:48 -0300

    Adicionando alts nas demais imagens

--- src/assets/js/games.js
+++ src/assets/js/games.js
@@ -103,12 +103,21 @@ const hollowKnight = new Game(
 const undertale = new Game(
   "Undertale",
   "RPG / Indie / Aventura",
-  "src/assets/imgs/logos/Undertale-Logo.png",
-  "src/assets/imgs/character/frisk-undertale.webp",
+  {
+    url: "src/assets/imgs/logos/Undertale-Logo.png",
+    alt: "Imagem do logo de Undertale",
+  },
+  {
+    url: "src/assets/imgs/character/frisk-undertale.webp",
+    alt: "Imagem de Frisk de Undertale",
+  },
   "src/assets/imgs/background-imgs/castle-img-undertale.png",
   "15 de setembro de 2015",
   "https://store.steampowered.com/app/391540/Undertale/",
-  "src/assets/imgs/creator-img/toby-fox.webp",
+  {
+    url: "src/assets/imgs/creator-img/toby-fox.webp",
+    alt: "Imagem do annoying dog como representação oficial do toby fox criador de undertale",
+  },
   "Undertale é um RPG inovador criado por Toby Fox, onde suas escolhas realmente importam. O jogo se passa em um mundo subterrâneo habitado por monstros, e você pode escolher poupar ou derrotar seus inimigos, influenciando profundamente a narrativa.",
   "A história acompanha uma criança humana que cai em um mundo subterrâneo habitado por monstros, isolado da superfície por uma barreira mágica. No início, a criança é acolhida por Toriel, uma criatura gentil que tenta protegê-la, mas logo precisa seguir sozinha em busca de uma saída. Durante essa jornada, o jogador conhece figuras cativantes como Sans, Papyrus, Undyne, Alphys, Mettaton e Asgore, cada um com sua própria história e motivações. Dependendo das escolhas do jogador, o enredo muda completamente, explorando temas como empatia, perdão e as consequências das ações. Undertale se destaca por quebrar a quarta parede e fazer o jogador refletir sobre suas decisões, transformando uma simples aventura em uma experiência emocional e memorável.",
   "Undertale é um jogo de RPG que combina elementos clássicos com mecânicas únicas de combate, onde o jogador pode escolher entre lutar ou poupar os inimigos. Durante as batalhas, o jogador controla um coração que representa sua alma e precisa desviar de ataques em um sistema que lembra jogos de tiro com muitos projéteis. Cada decisão influencia diretamente o rumo da história e o comportamento dos personagens, permitindo diferentes rotas, como a pacifista, onde ninguém é morto; a neutra, que mistura combate e misericórdia; e a genocida, onde todos os inimigos são eliminados. Além dos combates, o jogo apresenta diálogos cheios de humor, quebra-cabeças e interações marcantes com os personagens do subsolo.",
@@ -163,12 +172,21 @@ const undertale = new Game(
 const deltarune = new Game(
   "Deltarune",
   "RPG / Indie / Aventura",
-  "src/assets/imgs/logos/DeltaruneLogo.webp",
-  "src/assets/imgs/character/kris-deltarune.webp",
+  {
+    url: "src/assets/imgs/logos/DeltaruneLogo.webp",
+    alt: "Logo oficial do Deltarune",
+  },
+  {
+    url: "src/assets/imgs/character/kris-deltarune.webp",
+    alt: "Imagem de Kris de Deltarune",
+  },
   "src/assets/imgs/background-imgs/deltarune-bg.jpg",
   "31 de outubro de 2018",
   "https://store.steampowered.com/app/1671210/DELTARUNE/",
-  "src/assets/imgs/creator-img/toby-fox.webp",
+  {
+    url: "src/assets/imgs/creator-img/toby-fox.webp",
+    alt: "Imagem do annoying dog como representação oficial do toby fox criador de undertale",
+  },
   "Deltarune é o sucessor espiritual de Undertale, também criado por Toby Fox. O jogo apresenta um novo elenco de personagens e um sistema de combate aprimorado, mantendo o humor e a criatividade do original.",
   "O jogador assume o controle de Kris, um humano que vive em uma cidade habitada por monstros. Após um evento misterioso na escola, Kris e sua colega Susie caem em um mundo alternativo conhecido como Mundo das Trevas, onde encontram Ralsei, um príncipe que acredita que eles são heróis destinados a restaurar o equilíbrio entre a luz e a escuridão. A jornada do trio envolve enfrentar inimigos, resolver enigmas e formar laços de amizade enquanto tentam cumprir essa missão.",
   "A jogabilidade mantém o sistema de batalhas por turnos com a opção de lutar ou poupar os oponentes, mas agora em um formato de grupo, com cada personagem possuindo habilidades próprias. Durante os combates, o jogador também precisa desviar de ataques controlando um pequeno coração, e as escolhas de diálogo e ação influenciam a forma como os personagens reagem e se relacionam. Assim como em Undertale, Deltarune mistura momentos de humor e emoção, mas com uma narrativa mais linear e cinematográfica, focando no desenvolvimento dos personagens e nas relações entre eles. O jogo apresenta um tom mais misterioso e simbólico, deixando diversas perguntas em aberto sobre o passado e o destino de Kris, e promete expandir sua história em capítulos futuros.",
@@ -222,12 +240,21 @@ const deltarune = new Game(
 const silksong = new Game(
   "Hollow Knight: Silksong",
   "Metroidvania / Ação e Aventura / Plataforma 2D",
-  "src/assets/imgs/logos/hk-silksong-logo.webp",
-  "src/assets/imgs/character/hornet-hk-silksong.webp",
+  {
+    url: "src/assets/imgs/logos/hk-silksong-logo.webp",
+    alt: "Logo oficial de Hollow Knight Silksong",
+  },
+  {
+    url: "src/assets/imgs/character/hornet-hk-silksong.webp",
+    alt: "Imagem de Hornet protagonista de Hollow Knight Silksong",
+  },
   "src/assets/imgs/background-imgs/silksong-bg.jpg",
   "4 de setembro de 2025",
   "https://store.steampowered.com/app/1030300/Hollow_Knight_Silksong/",
-  "src/assets/imgs/creator-img/team-cherry-logo.png",
+  {
+    url: "src/assets/imgs/creator-img/team-cherry-logo.png",
+    alt: "Imagem da Team Cherry desenvolvedora de Hollow Knight",
+  },
   "Hollow Knight: Silksong é a sequência de Hollow Knight, desenvolvida pela Team Cherry. Desta vez, você controla Hornet, explorando um novo reino chamado Fiarlongo repleto de desafios, segredos e inimigos inéditos.",
   "Em Silksong, o jogador assume o controle de Hornet, a princesa protetora de Hallownest, que foi capturada e levada para o misterioso reino de Pharloom. Este novo mundo é descrito como 'assombrado por seda e música', oferecendo uma atmosfera única e repleta de segredos. Hornet deve enfrentar desafios e inimigos enquanto busca entender os motivos de sua captura e desvendar os mistérios que permeiam Fiarlongo.",
   "A jogabilidade de Silksong expande e refina os elementos do primeiro jogo. Hornet é mais ágil e versátil, equipada com uma agulha e linha que lhe conferem habilidades de combate e movimentação aprimoradas. O jogo apresenta mais de 165 inimigos diferentes, além de diversos NPCs que enriquecem a experiência. A exploração é incentivada por meio de um sistema de missões que recompensa o jogador com upgrades e habilidades adicionais. A dinâmica de combate é mais rápida e fluida, com Hornet podendo realizar ataques à distância e combos, proporcionando uma experiência mais intensa e estratégica.",
@@ -282,12 +309,21 @@ const silksong = new Game(
 const celeste = new Game(
   "Celeste",
   "Plataforma / Indie / Aventura",
-  "src/assets/imgs/logos/Celeste-logo.png",
-  "src/assets/imgs/character/madeline-celeste.png",
+  {
+    url: "src/assets/imgs/logos/Celeste-logo.png",
+    alt: "Logo oficial de Celeste",
+  },
+  {
+    url: "src/assets/imgs/character/madeline-celeste.png",
+    alt: "Imagem de Madeline de Celeste",
+  },
   "src/assets/imgs/background-imgs/celeste-background.webp",
   "25 de janeiro de 2018",
   "https://store.steampowered.com/app/504230/Celeste/",
-  "src/assets/imgs/creator-img/maddy-make-games.png",
+  {
+    url: "src/assets/imgs/creator-img/maddy-make-games.png",
+    alt: "Imagem da logo de maddy make games desenvolvedora de Celeste",
+  },
   "Celeste é um jogo de plataforma desafiador sobre escalar a Montanha Celeste. Você controla Madeline em sua jornada de autodescoberta, enfrentando desafios físicos e emocionais em fases criativas e difíceis.",
   "Celeste tem como história a jornada de Madeline, uma jovem que decide escalar a montanha Celeste. Mais do que apenas enfrentar desafios físicos, ela precisa lidar com seus próprios medos, ansiedade e inseguranças. Durante a escalada, Madeline encontra uma versão sombria de si mesma, que representa suas dúvidas internas e momentos de fraqueza, sendo necessário confrontar essa parte de si para avançar. O jogo aborda temas como superação pessoal, saúde mental e autoaceitação, criando uma narrativa emocionalmente envolvente que conecta o jogador com a personagem.",
   "Na jogabilidade, Celeste é um jogo de plataforma 2D focado em precisão e timing. O jogador precisa dominar saltos, dashes rápidos e escaladas estratégicas para superar obstáculos desafiadores. Cada fase é desenhada para testar reflexos e paciência, mas checkpoints frequentes garantem que o progresso não se torne frustrante. Além disso, há coletáveis opcionais, como morangos, que oferecem desafios extras e recompensas para jogadores mais habilidosos. O design das fases combina dificuldade justa com sensação de conquista ao completar cada trecho.",
@@ -342,12 +378,21 @@ const celeste = new Game(
 const cuphead = new Game(
   "Cuphead",
   "Run and Gun / Plataforma / Indie",
-  "src/assets/imgs/logos/cuphead-logo.png",
-  "src/assets/imgs/character/Cuphead.webp",
+  {
+    url: "src/assets/imgs/logos/cuphead-logo.png",
+    alt: "Logo oficial de Cuphead",
+  },
+  {
+    url: "src/assets/imgs/character/Cuphead.webp",
+    alt: "Imagem do Cuphead protagonista do jogo cuphead",
+  },
   "src/assets/imgs/background-imgs/cuphead-bg.avif",
   "29 de setembro de 2017",
   "https://store.steampowered.com/app/268910/Cuphead/",
-  "src/assets/imgs/creator-img/StudioMDHR.webp",
+  {
+    url: "src/assets/imgs/creator-img/StudioMDHR.webp",
+    alt: "Logo do studio MDHR desenvolvedor de Cuphead",
+  },
   "Cuphead é um jogo de ação inspirado em desenhos animados dos anos 1930. Com visual e trilha sonora retrô, o jogo é conhecido por sua dificuldade elevada e batalhas contra chefes criativos.",
   "Cuphead apresenta uma história simples, mas cativante: os irmãos Cuphead e Mugman perdem uma aposta com o Diabo e precisam coletar as almas de outros devedores para salvar suas próprias vidas. A narrativa é reforçada pelo estilo visual único do jogo, inspirado em desenhos animados dos anos 1930, e pela trilha sonora de jazz, que dão personalidade e charme ao universo do jogo, tornando cada fase memorável mesmo que a história seja direta.",
   "Quanto à jogabilidade, Cuphead combina fases de plataforma com batalhas intensas contra chefes. O jogo exige reflexos rápidos e memorização de padrões, pois cada inimigo possui ataques e comportamentos específicos. O combate envolve tiros, ataques especiais e esquivas, e as fases de plataforma exigem precisão para superar obstáculos e inimigos menores. O jogo é famoso por sua dificuldade elevada, mas cada vitória traz grande sensação de conquista, e o estilo artístico torna cada batalha visualmente impressionante.",
@@ -402,12 +447,21 @@ const cuphead = new Game(
 const shovelKnight = new Game(
   "Shovel Knight",
   "Plataforma / Ação / Indie",
-  "src/assets/imgs/logos/shovel-knight-logo.png",
-  "src/assets/imgs/character/shovel-knight.webp",
+  {
+    url: "src/assets/imgs/logos/shovel-knight-logo.png",
+    alt: "Logo oficial de Shovel Knight",
+  },
+  {
+    url: "src/assets/imgs/character/shovel-knight.webp",
+    alt: "Imagem do Shovel Knight protagonista de Shovel Knight",
+  },
   "src/assets/imgs/background-imgs/shovel-knight-background.jpg",
   "26 de junho de 2014",
   "https://store.steampowered.com/app/250760/Shovel_Knight_Treasure_Trove/",
-  "src/assets/imgs/creator-img/Yacht_Club_Games.webp",
+  {
+    url: "src/assets/imgs/creator-img/Yacht_Club_Games.webp",
+    alt: "Logo da Yacht Club Games desenvolvedora de Shovel Knight",
+  },
   "Shovel Knight é um jogo de plataforma retrô que homenageia clássicos dos anos 8 bits. Você controla o Cavaleiro da Pá, enfrentando inimigos e chefes em fases desafiadoras para salvar sua amada Shield Knight.",
   "Shovel Knight conta a história de um cavaleiro que usa uma pá como arma e embarca em uma missão para resgatar sua amada Shield Knight e derrotar a Orde das Enchantresses. A narrativa combina elementos de fantasia clássica, humor e homenagens aos jogos de 8 bits, criando um universo nostálgico, mas com personalidade própria e momentos cômicos que enriquecem a experiência do jogador.",
   "Na jogabilidade, Shovel Knight mistura plataforma 2D, exploração e combate. O jogador enfrenta inimigos usando a pá, descobre tesouros escondidos e enfrenta chefes com padrões distintos que exigem estratégia e atenção. O mapa do jogo permite progressão não-linear, com níveis e áreas que podem ser revisitados conforme novos itens ou habilidades são adquiridos. Além disso, o jogo combina desafios de plataforma clássicos com mecânicas de combate refinadas, tornando a experiência tanto acessível para iniciantes quanto satisfatória para jogadores que buscam desafios mais profundos.",
