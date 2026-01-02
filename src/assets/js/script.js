9fdb97a85d0edd5368ff3db941683dea3c261bc7
Erick Fellipe <efrc22br@gmail.com>
2026-01-02 15:22:26 -0300

    Reorganizado lista de jogos no select

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -18,6 +18,8 @@ const searchBtnMobile = document.querySelector(".search-btn-mobile");
 const errorMessage = document.querySelector(".message");
 const errorMessageMobile = document.querySelector(".message-mobile");
 
+const gameLists = document.querySelectorAll(".game-list");
+
 const gameLogo = document.getElementById("gameLogo");
 const gameCharacter = document.getElementById("gameCharacter");
 const gameType = document.querySelector(".game-type");
@@ -71,6 +73,33 @@ tabBtns.forEach((button, i) => {
     });
   });
 });
+
+async function sortGames() {
+  try {
+    const response = await fetch("src/assets/data/games.json");
+    const data = await response.json();
+
+    const games = [];
+
+    data.games.forEach((game) => games.push(game.name));
+
+    games.sort((a, b) => a.localeCompare(b));
+
+    gameLists.forEach((list) => {
+      list.innerHTML = "";
+      games.forEach((g) => {
+        const option = document.createElement("option");
+        option.value = g;
+        list.appendChild(option);
+      });
+    });
+  } catch (error) {
+    alert("ERRO AO PREENCHER LISTA DE JOGOS");
+    console.log(error);
+  }
+}
+
+sortGames();
 
 async function showInfoBySearch() {
   const searchBar = gameBar || gameBarMobile;
